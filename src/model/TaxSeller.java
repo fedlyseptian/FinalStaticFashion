8bc84e6fd86334d24ca81fe3affced9bcaecea5f
TimothyRay <TimothyRay@192.168.100.42>
2020-10-12 13:05:56 +0700

    tambah class taxseller

---
+++ src/model/TaxSeller.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author TimothyRay
+ */
+public class TaxSeller {
+    private double taxValue;
+
+    public TaxSeller(double taxValue) {
+        this.taxValue = taxValue;
+    }
+    
+    public double getTaxValue() {
+        return taxValue;
+    }
+
+    public void setTaxValue(double taxValue) {
+        this.taxValue = taxValue;
+    }
+}
