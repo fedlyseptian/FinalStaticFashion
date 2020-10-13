fa175422d34688cd38059c9241c2fe94f16235aa
Jeddi212 <jedediahf51@gmail.com>
2020-10-13 09:54:08 +0700

    Edit constructor parameter order

--- src/model/member.java
+++ src/model/member.java
@@ -5,16 +5,60 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Asus
  */
-public class member extends Person {
+public class Member extends Person {
 
-    public member(String username, String password, char gender, String email, int d, int m, int y) {
+    String name;
+    String address;
+    double point;
+    ArrayList<Transactions> listTransaction;
+
+    public Member(String username, String password, char gender, String email, int d, int m, int y, String name, String address, double point, ArrayList<Transactions> listTransaction) {
         super(username, password, gender, email, d, m, y);
+        this.name = name;
+        this.address = address;
+        this.point = point;
+        this.listTransaction = listTransaction;
     }
 
-    public member() {
+    public Member(){
+
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getPoint() {
+        return point;
+    }
+
+    public ArrayList<Transactions> getListTransaction() {
+        return listTransaction;
+    }
+
+    public void setListTransaction(ArrayList<Transactions> listTransaction) {
+        this.listTransaction = listTransaction;
+    }
+    
+    
+    
+    
 }
