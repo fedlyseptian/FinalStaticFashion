9357c3351bd3f82a673bb4d771a0f052d833e1ba
TimothyRay18 <timothy.ray1802@gmail.com>
2020-11-13 22:06:19 +0700

    pindahkan fungsi md5 ke Controller.java

--- src/controller/ControllerDatabase.java
+++ src/controller/ControllerDatabase.java
@@ -5,13 +5,8 @@
  */
 package controller;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import main.Main;
 import model.Member;
@@ -122,26 +117,26 @@ public class ControllerDatabase {
         return (listUsername);
     }
 
-    public static String md5Java(String message)
-    {
-        String digest = null;
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] hash = md.digest(message.getBytes("UTF-8"));
-            //merubah byte array ke dalam String Hexadecimal
-            StringBuilder sb = new StringBuilder(2*hash.length);
-            for(byte b : hash)
-            {
-                sb.append(String.format("%02x", b&0xff));
-            }
-            digest = sb.toString();
-        } catch (UnsupportedEncodingException ex)
-        {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NoSuchAlgorithmException ex)
-        {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return digest;
-    }
+//    public static String md5Java(String message)
+//    {
+//        String digest = null;
+//        try {
+//            MessageDigest md = MessageDigest.getInstance("MD5");
+//            byte[] hash = md.digest(message.getBytes("UTF-8"));
+//            //merubah byte array ke dalam String Hexadecimal
+//            StringBuilder sb = new StringBuilder(2*hash.length);
+//            for(byte b : hash)
+//            {
+//                sb.append(String.format("%02x", b&0xff));
+//            }
+//            digest = sb.toString();
+//        } catch (UnsupportedEncodingException ex)
+//        {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (NoSuchAlgorithmException ex)
+//        {
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return digest;
+//    }
 }
